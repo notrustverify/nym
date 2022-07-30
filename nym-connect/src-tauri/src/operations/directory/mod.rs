1464e5de1edb939589a88e3ca518f44a57251006
cgi-bin/ <6095048+sven-hash@users.noreply.github.com>
2022-07-30 16:45:35 +0200

    redefine services provider

--- nym-connect/src-tauri/src/operations/directory/mod.rs
+++ nym-connect/src-tauri/src/operations/directory/mod.rs
@@ -2,7 +2,7 @@ use crate::error::Result;
 use crate::models::DirectoryService;
 
 static SERVICE_PROVIDER_WELLKNOWN_URL: &str =
-    "https://nymtech.net/.wellknown/connect/service-providers.json";
+    "https://gist.githubusercontent.com/sven-hash/0e388d1cf305ffe94c9076659c2595eb/raw/service-providers.json";
 
 #[tauri::command]
 pub async fn get_services() -> Result<Vec<DirectoryService>> {
